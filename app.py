e5651f7be2e8250e3365a2419225ff91b9095ff2
RadekRo <r.roclawski@ankara.pl>
2023-04-02 17:45:14 +0200

    Created template list.html file and routing in app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/list')
+def route_list():
+    return render_template("list.html")
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(
+        host='127.0.0.1',
+        port=5000,
+        debug=True,
+    )
