08e21dd0b8e22dba210b7824868868be13e88db5
glabojko <glabojko@gmail.com>
2023-04-03 22:09:37 +0200

    question added, adding functions to url managed

--- app.py
+++ app.py
@@ -10,9 +10,16 @@ def route_list():
     all_questions = data_handler.get_all_questions()
     return render_template("list.html", all_questions=all_questions)
 
+@app.route('/question/<id>')
+def route_question(id):
+    question = data_handler.get_question(id)
+    return render_template("question.html", question=question)
+
+
+
 if __name__ == '__main__':
     app.run()
 
 # for run the python's flask server
 # use> python -m flask run
-# for automatic changes and debugger use> python -m flask --debug run
+# for automatic changes and debugger use> "python -m flask --debug run"
--- data_handler.py
+++ data_handler.py
@@ -3,7 +3,7 @@ import csv, os
 DATA_FILE_PATH_ANSWER = 'data/answer.csv'
 DATA_FILE_PATH_QUESTION = 'data/question.csv'
 
-EXAMPLE = [["4", "1493068444", "45", "434", "science", "What is the purpose of living?"], ["4", "1495558444", "15", "423", "rock", "What is the purpose of dancin?"]]
+EXAMPLE = [["6", "1493068444", "45", "434", "science", "What is the purpose of living?"], ["9", "1495558444", "15", "423", "rock", "What is the purpose of dancin?"]]
 
 def import_questions():
     with open() as f:
@@ -17,6 +17,16 @@ def import_questions():
 
 def get_all_questions():
     return EXAMPLE
+
+def get_question(id):
+    for question in EXAMPLE:
+        if question[0] == id:
+            return question
+
+def get_href_question(id):
+    for question in EXAMPLE:
+        if question[0] == id:
+            return id
 
 
 
