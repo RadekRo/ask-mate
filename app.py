aa980566471cc21a022ced3acdb6cebb5084b764
RadekRo <r.roclawski@ankara.pl>
2023-04-03 18:50:28 +0200

    Corrected aphostophe

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ app = Flask(__name__)
 def route_list():
     return render_template("list.html")
 
-
 if __name__ == '__main__':
     app.run(debug = True)
 
 # for run the python's flask server
-# use python -m flask run
-# for automatic changes and debugger use: python -m flask --debug run
+# use> python -m flask run
+# for automatic changes and debugger use> python -m flask --debug run
