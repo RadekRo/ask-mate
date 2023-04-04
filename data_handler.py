0e896574228666112d70e6a3418160d175c9ff57
Radek Rocławski <r.roclawski@ankara.pl>
2023-04-04 11:18:37 +0200

    Plugged new file reader to all needed functions

--- data_handler.py
+++ data_handler.py
@@ -4,27 +4,29 @@ DATA_FILE_PATH_ANSWER = 'data/answer.csv'
 DATA_FILE_PATH_QUESTION = 'data/question.csv'
 FILES_EXTENSIONS = [".jpg", ".png", ".jpeg"]
 
-EXAMPLE = [["6", "1493068444", "45", "434", "science", "What is the purpose of living?"], ["9", "1495558444", "15", "423", "rock", "What is the purpose of dancin?"]]
-
-
 def import_questions(filename):
+    questions = list()
     with open(filename, 'r') as file:
         csvreader = csv.reader(file)
         for row in csvreader:
-            print(row)
+            questions.append(row)
+    return questions
 
 
 def get_all_questions():
-    return EXAMPLE
+    questions = import_questions(DATA_FILE_PATH_QUESTION)
+    return questions
 
 
 def get_question(id):
-    for question in EXAMPLE:
+    questions = import_questions(DATA_FILE_PATH_QUESTION)
+    for question in questions:
         if question[0] == id:
             return question
 
 def get_href_question(id):
-    for question in EXAMPLE:
+    questions = import_questions(DATA_FILE_PATH_QUESTION)
+    for question in questions:
         if question[0] == id:
             return id
 
