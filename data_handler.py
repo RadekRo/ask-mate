b1ebcfd8fbe3f74f3df1e7cd949245c2723613a5
Radek Rocławski <r.roclawski@ankara.pl>
2023-04-04 11:10:01 +0200

    Corrected file read including csv library reader() method

--- data_handler.py
+++ data_handler.py
@@ -2,21 +2,21 @@ import csv, os
 
 DATA_FILE_PATH_ANSWER = 'data/answer.csv'
 DATA_FILE_PATH_QUESTION = 'data/question.csv'
+FILES_EXTENSIONS = [".jpg", ".png", ".jpeg"]
 
 EXAMPLE = [["6", "1493068444", "45", "434", "science", "What is the purpose of living?"], ["9", "1495558444", "15", "423", "rock", "What is the purpose of dancin?"]]
 
-def import_questions():
-    with open() as f:
-        file = f.readlines()
-        questions = list()
-        for i in file:
-            question = i.rstrip("\n").split(",")
-            questions.append(question)
-        for question in questions:
-            print(question)
+
+def import_questions(filename):
+    with open(filename, 'r') as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            print(row)
+
 
 def get_all_questions():
     return EXAMPLE
+
 
 def get_question(id):
     for question in EXAMPLE:
@@ -29,5 +29,5 @@ def get_href_question(id):
             return id
 
 
-
+import_questions(DATA_FILE_PATH_QUESTION)
 
